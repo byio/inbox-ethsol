51e988eed0b23e254fb94da1c57fae9d158083d9
byio <ben@benyang.io>
2018-03-07 18:13:53 +0800

    refactor promise to async/await

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -5,18 +5,18 @@ const Web3 = require('web3'); // constructor function
 // create instance of web3 with ganache provider
 const web3 = new Web3(ganache.provider());
 
+// Declare vars
+let accounts;
+
 // Testing with Mocha
-beforeEach(() => {
+beforeEach(async () => {
   // fetch all accounts from ganache network
-  web3.eth.getAccounts()
-          .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-          });
+  accounts = await web3.eth.getAccounts();
   // use one of the returned accounts to deploy contract
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-
+    console.log(accounts);
   });
 });
