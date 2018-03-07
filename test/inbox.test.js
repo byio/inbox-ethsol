1ca879461bc623e741794c82ab85337eb36aed6e
byio <ben@benyang.io>
2018-03-07 18:11:16 +0800

    fetch list of ethereum accounts generated on ganache test nework using web3 instance beforeEach Inbox test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -4,3 +4,19 @@ const Web3 = require('web3'); // constructor function
 
 // create instance of web3 with ganache provider
 const web3 = new Web3(ganache.provider());
+
+// Testing with Mocha
+beforeEach(() => {
+  // fetch all accounts from ganache network
+  web3.eth.getAccounts()
+          .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+          });
+  // use one of the returned accounts to deploy contract
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+
+  });
+});
