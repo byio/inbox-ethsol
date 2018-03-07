6a2bad5ef69a7838fc53f2e109680ec087fe8e0f
byio <ben@benyang.io>
2018-03-07 18:54:16 +0800

    deploy new ethereum contract using web3 beforeEach Inbox test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,21 +2,37 @@ const assert = require('assert');
 const ganache = require('ganache-cli'); // ethereum RPC for testing
 const Web3 = require('web3'); // constructor function
 
+// Import Compiled Solidity Contract
+const { interface, bytecode } = require('../compile');
+
 // create instance of web3 with ganache provider
 const web3 = new Web3(ganache.provider());
 
 // Declare vars
 let accounts;
+let inbox;
 
 // Testing with Mocha
 beforeEach(async () => {
   // fetch all accounts from ganache network
   accounts = await web3.eth.getAccounts();
   // use one of the returned accounts to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+                        .deploy({
+                          data: bytecode,
+                          arguments: [
+                            'Hi there!'
+                          ]
+                        })
+                        .send({
+                          from: accounts[0],
+                          gas: 1000000
+                        });
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
     console.log(accounts);
+    console.log(inbox);
   });
 });
